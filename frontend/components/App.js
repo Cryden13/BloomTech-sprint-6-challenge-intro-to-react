1114e11e9317bc680a452edbad69e42f88de3144
Connor Rideout <connorrideout13@gmail.com>
2024-05-28 19:00:39 -0400

    completed Sprint 6 challenge

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -6,15 +6,35 @@ const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
-  return (
-    <div>
-      <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
-    </div>
-  )
+    // ❗ Create state to hold the data from the API
+    const [parsedData, setParsedData] = useState([])
+    // ❗ Create effects to fetch the data and put it in state
+    useEffect(() => {
+        Promise.all([axios.get(urlPeople), axios.get(urlPlanets)])
+            .then(([{ data: peopleData }, { data: planetsData }]) => {
+                // console.log(peopleData)
+                // console.log(planetsData)
+                const mapData = peopleData.map(person => {
+                    const { name: planetName } = planetsData.find(planet => planet.id === person.homeworld)
+                    const { id, name } = person
+                    return { id, name, homeworld: planetName }
+                })
+                setParsedData(mapData)
+                // console.log(mapData)
+            })
+    }, [])
+
+    return (
+        <div>
+            <h2>Star Wars Characters</h2>
+            <p>See the README of the project for instructions on completing this challenge</p>
+            {/* ❗ Map over the data in state, rendering a Character at each iteration */
+                parsedData.map(({ id, name, homeworld }) => {
+                    return <Character key={id} charName={name} homeworld={homeworld} />
+                })
+            }
+        </div>
+    )
 }
 
 export default App
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  // ❗ Create a "toggle" click handler to show or remove the homeworld
-  return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
-    </div>
-  )
+function Character({ charName, homeworld }) { // ❗ Add the props
+    // ❗ Create a state to hold whether the homeworld is rendering or not
+    const [renderHomeworld, setRenderHomeworld] = useState(false)
+    // ❗ Create a "toggle" click handler to show or remove the homeworld
+    const toggleHomeworld = () => {
+        setRenderHomeworld(!renderHomeworld)
+    }
+    return (
+        <div className="character-card" onClick={toggleHomeworld}>
+            {/* Use the same markup with the same attributes as in the mock */}
+            <h3 className='character-name'>{charName}</h3>
+            {renderHomeworld && <p>Planet: <span className='character-planet'>{homeworld}</span></p>}
+        </div>
+    )
 }
 
 export default Character
